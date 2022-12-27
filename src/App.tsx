fd6d3a02eff30beaa20497d29016028aeac1ebad
Achette <i.achette@live.com>
2022-12-27 12:20:44 -0300

    Feat: added new route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Homepage } from './routes/Homepage';
 import { Home } from './routes/Homepage/Home';
+import { Start } from './routes/Homepage/Start';
 
 
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path={`/`} element={<Homepage />}>
           <Route index element={<Home />} />
+          <Route path='start' element={<Start />} />
         </Route>
       </Routes>
     </BrowserRouter>
