be5cba5df80bc5ca06720ed88c4397b2556c2dc2
Achette <i.achette@live.com>
2022-12-27 12:15:42 -0300

    Feat: created Start page and style

---
+++ src/routes/Homepage/Start/index.tsx
@@ -0,0 +1,10 @@
+import { Card } from "../../../components";
+import './style.css'
+
+export const Start = () => {
+  return (
+    <div className="start-container">
+      <Card />
+    </div>
+  );
+};
