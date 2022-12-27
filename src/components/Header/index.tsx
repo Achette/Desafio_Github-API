22435ee277d072cefe95d2a81df5aa57f5909524
Achette <i.achette@live.com>
2022-12-27 10:58:17 -0300

    Feat: created Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import './style.css'
+
+
+export const Header = () => {
+
+    return (
+        <header className="header-container">
+            <nav className="header-content">
+                <h1>Github API</h1>
+            </nav>
+        </header>
+    )
+}
