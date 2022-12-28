e65ee253a1e8980acfe0a4691972fc43b78a82bd
Achette <i.achette@live.com>
2022-12-28 17:43:14 -0300

    Feat: created api get user method

---
+++ src/services/api.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+import { BASE_URL } from "../constants/system";
+
+export const ApiGithub = {
+  getByUserName: async (user: string) => {
+    const response = await axios.get(`${BASE_URL}/${user}`);
+    return response;
+  },
+};
