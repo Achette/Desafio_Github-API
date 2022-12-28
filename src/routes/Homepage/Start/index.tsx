1e1dc495ce16cf8f723bc5c5d20891365481ce99
Achette <i.achette@live.com>
2022-12-28 17:42:14 -0300

    Feat: created Start Page

--- src/routes/Homepage/Start/index.tsx
+++ src/routes/Homepage/Start/index.tsx
@@ -1,10 +1,38 @@
-import { Card } from "../../../components";
-import './style.css'
+import React from "react";
+import { Card, RequestCard } from "../../../components";
+import { ApiGithub } from "../../../services/api";
+import "./style.css";
 
 export const Start = () => {
+  const [data, setData] = React.useState();
+  const [user, setUser] = React.useState<string>("");
+  const [error, setError] = React.useState(false);
+
+  const captureUser = (user: string) => {
+    setUser(user);
+  };
+
+  const fetchUser = React.useCallback(async () => {
+    if (user !== "") {
+      setError(false);
+      await ApiGithub.getByUserName(user)
+        .then((response) => response.data)
+        .then((res) => setData(res))
+        .catch((error) => setError(true));
+    }
+  }, [user]);
+
+  React.useEffect(() => {
+    fetchUser();
+  }, [fetchUser]);
+
   return (
     <div className="start-container">
-      <Card />
+      <div className="start-content">
+        <Card captureUser={captureUser} />
+        {data && !error ? <RequestCard response={data} /> : null}
+        <span className="error-message">{error && <p>Erro ao buscar usuário</p>}</span>
+      </div>
     </div>
   );
 };
