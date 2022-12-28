2a8c64311550a70689ce246c505e82a0b5ccd4d6
Achette <i.achette@live.com>
2022-12-28 17:36:11 -0300

    Refact: removed unused route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Homepage } from './routes/Homepage';
-import { Home } from './routes/Homepage/Home';
-import { Start } from './routes/Homepage/Start';
-
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Homepage } from "./routes/Homepage";
+import { Home } from "./routes/Homepage/Home";
+import { Start } from "./routes/Homepage/Start";
 
 function App() {
   return (
@@ -12,7 +10,7 @@ function App() {
       <Routes>
         <Route path={`/`} element={<Homepage />}>
           <Route index element={<Home />} />
-          <Route path='start' element={<Start />} />
+          <Route path="start/" element={<Start />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
