97aa6ae25a9fdc99ac8bcd1d829cd8820a853904
Achette <i.achette@live.com>
2022-12-28 17:39:41 -0300

    Refact: moved type to another file

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,7 @@
+import { buttonProps } from "../../types";
 import "./style.css";
 
-type buttonProps = {
-  text: string;
-  onClick?: () => void;
-  type: "submit" | "button"
-};
+
 
 export const Button = ({ text, onClick, type }: buttonProps) => {
   return <button type={`${type}`} onClick={onClick}>{text}</button>;
